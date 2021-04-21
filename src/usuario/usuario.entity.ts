6a58c1402271510da133a990c71dd93ca2a91779
Emerson Oliveira <emerson@fourcicle.com.br>
2021-04-21 11:58:39 -0300

    usando interceptors para manipular os tipos de dados de entrada e saida

--- src/usuario/usuario.entity.ts
+++ src/usuario/usuario.entity.ts
@@ -1,9 +1,13 @@
+import { Exclude, Expose } from 'class-transformer';
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 import { IsNomeUsuarioUnico } from './is-nome-de-usuario-unico.validator';
 
 export class Usuario {
   id: number;
 
+  @Expose({
+    name: 'userName',
+  })
   @IsNomeUsuarioUnico({
     message: 'nomeDeUsuario precisa ser único',
   })
@@ -13,15 +17,31 @@ export class Usuario {
   @IsString()
   nomeDeUsuario: string;
 
+  @Expose({
+    name: 'email',
+  })
   @IsEmail()
   email: string;
 
+  @Expose({
+    name: 'password',
+  })
+  @Exclude({
+    toPlainOnly: true,
+  })
   @IsNotEmpty()
   senha: string;
 
+  @Expose({
+    name: 'fullName',
+  })
   @IsNotEmpty({
     message: 'nomeCompleto é obrigatório',
   })
   nomeCompleto: string;
+
+  @Expose({
+    name: 'joinDate',
+  })
   dataDeEntrada: Date;
 }
